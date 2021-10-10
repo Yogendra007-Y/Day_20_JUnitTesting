0289d51b9824ef97143d9b545431f2e8b978a723
yogkri <dharmayog91@gmail.com>
2021-10-10 20:44:15 +0530

    Used Junit Test To Test EmailId Format

--- src/main/java/com/Bridgelabz/userRegistrationFormat.java
+++ src/main/java/com/Bridgelabz/userRegistrationFormat.java
@@ -6,22 +6,31 @@ import java.util.regex.Pattern;
 public class userRegistrationFormat {
     Pattern pattern;
     Matcher matcher;
-    /* method firstName to validate the first name of the user
+    /* method firstName to validate first name format
        @param name return boolean value
      */
-    public boolean firstName(String name){
+    public boolean firstName(String FirstName){
         String regex = "^[A-Z]{1}[a-z]{2,}";
         pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
+        matcher = pattern.matcher(FirstName);
         return matcher.matches();
     }
-    /* methodlastName to validation of the name value
+    /* method lastName to validate last name format
        @param name return boolean value
      */
-    public boolean lastName(String name){
+    public boolean lastName(String LastName){
         String regex = "^[A-Z]{1}[a-z]{2,}";
         pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
+        matcher = pattern.matcher(LastName);
+        return matcher.matches();
+    }
+    /* method emailValidation to validate email format0
+       @param name return boolean value
+     */
+    public boolean emailValidation(String Email){
+        String regx = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z][2,3]){0,1}$";
+        pattern = Pattern.compile(regx);
+        matcher = pattern.matcher(Email);
         return matcher.matches();
     }
 }
