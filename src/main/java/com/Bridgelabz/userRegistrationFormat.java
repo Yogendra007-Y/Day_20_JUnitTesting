b317885c0c6689bb68fc15befaf67e388d24ffd3
yogkri <dharmayog91@gmail.com>
2021-10-10 20:40:28 +0530

    Used Junit Test To Test Last Name Format

--- src/main/java/com/Bridgelabz/userRegistrationFormat.java
+++ src/main/java/com/Bridgelabz/userRegistrationFormat.java
@@ -7,9 +7,18 @@ public class userRegistrationFormat {
     Pattern pattern;
     Matcher matcher;
     /* method firstName to validate the first name of the user
-     * @param name return boolean value
+       @param name return boolean value
      */
     public boolean firstName(String name){
+        String regex = "^[A-Z]{1}[a-z]{2,}";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+    /* methodlastName to validation of the name value
+       @param name return boolean value
+     */
+    public boolean lastName(String name){
         String regex = "^[A-Z]{1}[a-z]{2,}";
         pattern = Pattern.compile(regex);
         matcher = pattern.matcher(name);
