ca5a3820b0d79bfe371dd1c9d0cba304b32d77e2
yogkri <dharmayog91@gmail.com>
2021-10-10 20:56:01 +0530

    Used Junit Test To Test Phone Number Format

--- src/main/java/com/Bridgelabz/userRegistrationFormat.java
+++ src/main/java/com/Bridgelabz/userRegistrationFormat.java
@@ -7,7 +7,7 @@ public class userRegistrationFormat {
     Pattern pattern;
     Matcher matcher;
     /* method firstName to validate first name format
-       @param name return boolean value
+       @param FirstName return boolean value
      */
     public boolean firstName(String FirstName){
         String regex = "^[A-Z]{1}[a-z]{2,}";
@@ -16,7 +16,7 @@ public class userRegistrationFormat {
         return matcher.matches();
     }
     /* method lastName to validate last name format
-       @param name return boolean value
+       @param LastName return boolean value
      */
     public boolean lastName(String LastName){
         String regex = "^[A-Z]{1}[a-z]{2,}";
@@ -25,12 +25,21 @@ public class userRegistrationFormat {
         return matcher.matches();
     }
     /* method emailValidation to validate email format0
-       @param name return boolean value
+       @param Email return boolean value
      */
     public boolean emailValidation(String Email){
         String regx = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z][2,3]){0,1}$";
         pattern = Pattern.compile(regx);
         matcher = pattern.matcher(Email);
+        return matcher.matches();
+    }
+    /* method  numberValidationto validate phone no format
+     * @param PhoneNo return boolean value
+     */
+    public boolean numberValidation(String PhoneNo){
+        String regx = "^[0-9]{2}\\s[0-9]{10}";
+        pattern = Pattern.compile(regx);
+        matcher = pattern.matcher(PhoneNo);
         return matcher.matches();
     }
 }
